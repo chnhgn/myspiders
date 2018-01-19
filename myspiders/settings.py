f3b0b34d11a8f501b02d9fdf2e0374634785201c
Eddy Hu <guoning.hu@sas.com>
2018-01-19 15:06:15 +0800

    Store the items in mongodb

--- myspiders/settings.py
+++ myspiders/settings.py
@@ -64,9 +64,12 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'myspiders.pipelines.MyspidersPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'myspiders.pipelines.MyspidersPipeline': 300,
+}
+
+MONGO_URI='localhost'
+MONGO_DB='myspiders'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
